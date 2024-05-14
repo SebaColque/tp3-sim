ffc74aa02c538e849a4b64a50475e13f990ee87d
SebaColque <sebacolque06@gmail.com>
2024-05-13 23:56:24 -0300

    pages?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://SebaColque.github.io/tp3-sim',
+  base: 'https://sebacolque.github.io/tp3-sim',
 })
